defddc275f5ec41d6cfbd823658756263573d7d1
Antonioisrooted <antoniommaina@gmail.com>
2020-05-12 15:24:20 +0300

    News file

--- src/main/java/Dao/NewsDao.java
+++ src/main/java/Dao/NewsDao.java
@@ -1,4 +1,26 @@
 package Dao;
 
+import models.Departments;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+    void add(News news);
+    void addDepartmentsToNews(News news, Departments departments);
+
+    //read
+    List<News> getAll();
+    List<Departments> getAllDepartmentsByNews(int departments_id);
+
+
+
+
+    //update
+    //void update(String general_news, String department_news);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
