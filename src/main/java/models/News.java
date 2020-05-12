cbc91efd8405106fa0074d145268830a50bab91b
Antonioisrooted <antoniommaina@gmail.com>
2020-05-12 15:30:16 +0300

    News model files

---
+++ src/main/java/models/News.java
@@ -0,0 +1,42 @@
+package models;
+
+public class News {
+    /* name, position in the company, their roles;  */
+    private String general_news;
+    private String department_news;
+    private int id;
+
+
+    public News(String general_news, String department_news) {
+        this.general_news = general_news;
+        this.department_news = department_news;
+    }
+
+    public News(String department_news) {
+        this.department_news = department_news;
+    }
+
+    public String getGeneral_news() {
+        return general_news;
+    }
+
+    public void setGeneral_news(String general_news) {
+        this.general_news = general_news;
+    }
+
+    public String getDepartment_news() {
+        return department_news;
+    }
+
+    public void setDepartment_news(String department_news) {
+        this.department_news = department_news;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
